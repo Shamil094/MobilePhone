602613ab4058a718dfd41869c34617618795373f
Shamil Celilzade <shamillcelilzade@gmail.com>
2022-12-16 01:36:07 +0400

    Add project files.

---
+++ Mobile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+    public abstract class Camera  //abstraction
+    {
+        public abstract void GetPicture();
+        public abstract void GetVideo();
+    }
+
+    internal class Mobile : Camera  //inheritance
+    {
+        public Mobile(int memory)
+        {
+            Memory = memory;
+        }
+        int Memory;
+        private int data = 0;
+        private int Data   //encapsulation
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public override void GetPicture()
+        {
+
+            if (data < Memory)
+            {
+                Console.WriteLine("I have get a Pic");
+                Data++;
+            }
+            else
+            {
+                Console.WriteLine("Memory is full");
+            }
+        }
+
+        public override void GetVideo()
+        {
+            Console.WriteLine("I have get a Video");
+            Data += 2;
+        }
+        public void GetVideo(int maxminute)   //polymorphizm
+        {
+            if (maxminute > 10)
+            {
+                Console.WriteLine("STOPED");
+            }
+            else
+            {
+                Data += 2;
+                Console.WriteLine("Have Registered");
+            }
+        }
+        public void GetVideo(int maxminute, int gb)   //polymorphizm overload
+        {
+            if (maxminute > 10 || (gb + data) > Memory)
+            {
+                Console.WriteLine("stoped");
+            }
+            else
+            {
+                Data += gb;
+                Console.WriteLine("Have Registered");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobile samsung = new Mobile(8);
+            Mobile iphone = new Mobile(128);
+
+            samsung.GetVideo();
+            samsung.GetVideo(5);
+            samsung.GetVideo(3, 5);
+            samsung.GetPicture();
+            
+
+            Console.Read();
+        }
+    }
+}
